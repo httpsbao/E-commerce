597b452a19d951455b9cc088519ac396c0a190c6
httpsbao <httpsbao@outlook.com>
2020-06-22 16:55:04 +0800

    vue-express

---
+++ vue-online-shop-frontend/src/pages/Cart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="title">
+      <h1>{{msg}}</h1>
+    </div>
+    <template v-for="product in cart">
+      <product-item :product="product" :key="product._id"></product-item>
+    </template>
+  </div>
+</template>
+
+<style>
+.product {
+  border-bottom: 1px solid black;
+}
+
+.product__image {
+  width: 100px;
+  height: 100px;
+}
+</style>
+
+<script>
+import ProductItem from '@/components/products/ProductItem.vue';
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: 'Welcome to the Cart Page'
+      }
+    },
+    computed: {
+      cart() {
+        return this.$store.state.cart;
+      }
+    },
+    components: {
+      'product-item': ProductItem
+    }
+  }
+</script>
