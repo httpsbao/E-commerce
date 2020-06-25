a0f20bf4f250357a4b4a21851a81048c5bd410c3
httpsbao <httpsbao@outlook.com>
2020-06-25 13:55:07 +0800

    editable

--- vue-online-shop-frontend/src/components/products/ProductForm.vue
+++ vue-online-shop-frontend/src/components/products/ProductForm.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="productInfo">
-    <el-form class="form" ref="form" :model="model" label-width="180px">
+    <el-form
+    class="form"
+    ref="form"
+    label-width="180px"
+    v-loading="loading"
+    element-loading-text="拼命加载中"
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)">
       <el-form-item label="Name">
-        <el-input v-model="model.name"></el-input>
+        <el-input v-model="modelData.name"></el-input>
       </el-form-item>
       <el-form-item label="Price">
-        <el-input v-model="model.price"></el-input>
+        <el-input v-model="modelData.price"></el-input>
       </el-form-item>
       <el-form-item label="Manufacturer ">
-        <el-select v-model="model.manufacturer.name" clearable placeholder="请选择制造商">
+        <el-select
+          v-model="modelData.manufacturer.name"
+          clearable
+          placeholder="请选择制造商"
+        >
           <el-option
             v-for="manufacturer in manufacturers"
             :key="manufacturer._id"
             :label="manufacturer.name"
-            :value="manufacturer.name">
+            :value="manufacturer.name"
+          >
           </el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="Image ">
-        <el-input v-model="model.image"></el-input>
+        <el-input v-model="modelData.image"></el-input>
       </el-form-item>
       <el-form-item label="Description ">
-        <el-input type="textarea" v-model="model.description"></el-input>
+        <el-input type="textarea" v-model="modelData.description"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button v-if="isEditing" type="primary" @click="onSubmit">Update Product</el-button>
+        <el-button v-if="isEditing" type="primary" native-type="submit" @click="onSubmit"
+          >Update Product</el-button
+        >
         <el-button v-else @click="onSubmit">Add Product</el-button>
       </el-form-item>
     </el-form>
@@ -63,7 +77,7 @@
       </div>
     </div> -->
 
-    <!-- <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+  <!-- <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
       <div class="form-group">
         <label>Image</label>
         <input
@@ -87,8 +101,8 @@
       <div class="form-group new-button">
         <button class="button">
           <i class="fa fa-pencil"></i> -->
-          <!-- Conditional rendering for input text -->
-          <!-- <span v-if="isEditing">Update Product</span>
+  <!-- Conditional rendering for input text -->
+  <!-- <span v-if="isEditing">Update Product</span>
           <span v-else>Add Product</span>
         </button>
       </div>
@@ -98,16 +112,46 @@
 
 <script>
 export default {
-  props: ['model', 'manufacturers', 'isEditing'],
-  created() {
-    console.log(this.model)
+  data() {
+    return {
+      modelData: { manufacturer: { name: "" } }
+    };
   },
+  props: ["model", "manufacturers", "isEditing"],
+  created() {
+    const product = this.model;
+    this.modelData = { ...product, manufacturer: { ...product.manufacturer } };
+  },
+  watch: {
+    model(val, oldVal) {
+      this.modelData = val;
+    }
+  },
+  computed: {
+    loading() {
+      return this.$store.state.showLoader
+    }
+  },
+  // updated() {
+  //   // 等待后端完成商品信息的同步更新，设置loading为false
+  //   const newProductName = this.$store.getters.productById(this.model._id);
+  //   const isLoaded = this.modelData.name === newProductName;
+  //   if(isLoaded) {
+  //     this.loading = false;
+  //   }
+  // },
   methods: {
     onSubmit() {
-      this.$emit('save-product', this.model)
+      // 由于表单中只绑定了modelData.manufacturer.name，
+      // 缺少manufacturer._id,但是后端需要manufacturer整个对象,
+      // 所以需要将manufacturers中对应的manufacturer找出并覆盖到modelData中
+      const manufacturer = this.manufacturers.find(item => item.name === this.modelData.manufacturer.name);
+      this.modelData.manufacturer = manufacturer;
+
+      this.$emit("save-product", this.modelData);
     }
   }
-}
+};
 </script>
 <style>
 .productInfo {
--- vue-online-shop-frontend/src/store/mutations.js
+++ vue-online-shop-frontend/src/store/mutations.js
@@ -22,7 +22,7 @@ import {
   REMOVE_MANUFACTURER,
   REMOVE_MANUFACTURER_SUCCESS,
 } from './mutation-types';
-
+import { Message } from 'element-ui';
 
 export const productMutations = {
   [ALL_PRODUCTS](state) {
@@ -64,9 +64,10 @@ export const productMutations = {
       if (product._id === newProduct._id) {
         return newProduct;
       }
-
       return product;
-    })
+    });
+
+    state.product = newProduct;
   },
   [ADD_PRODUCT](state) {
     state.showLoader = true;
@@ -82,11 +83,19 @@ export const productMutations = {
 export const cartMutations = {
   [ADD_TO_CART](state, payload) {
     const { product } = payload;
-    state.cart.push(product)
+    state.cart.push(product);
+    Message({
+      message: '恭喜你，成功加入购物车！',
+      type: 'success'
+    })
   },
   [REMOVE_FROM_CART](state, payload) {
     const { productId } = payload
     state.cart = state.cart.filter(product => product._id !== productId)
+    Message({
+      message: '恭喜你，成功移除购物车！',
+      type: 'success'
+    })
   },
 };
 
@@ -129,9 +138,10 @@ export const manufacturerMutations = {
       if (manufacturer._id === newManufacturer._id) {
         return newManufacturer;
       }
-
       return manufacturer;
-    })
+    });
+
+    state.manufacturer=newManufacturer;
   },
   [ADD_MANUFACTURER](state) {
     state.showLoader = true;
